4a0283568c4d12e07cee4221d6cf2d0bab93253b
RVS <ratomir@live.com>
2015-06-25 20:22:38 +0200

    Prvo slanje.

---
+++ TouristGit/src/main/MainClass.java
@@ -0,0 +1,17 @@
+package main;
+
+import view.login.LoginForm;
+/**
+ * Klasa sadrzi funkciju koja pokrece aplikaciju
+ * 
+ * @author Grupa 4
+ *
+ */
+public class MainClass
+{
+
+	public static void main(String[] args)
+	{
+		new LoginForm();
+	}
+}
